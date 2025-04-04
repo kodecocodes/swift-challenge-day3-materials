04df1138b16a6a1133e3851084d41e6a3a5dd9c1
Sam Davies <sam@kodeco.com>
2025-04-04 15:09:56 +0100

    Adding day 3 materials

---
+++ Starter/Lesson-3-Demo-1-starter.swiftpm/ContentView.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Swift Apprentice: Fundamentals")
+            Text("Beginning programming with Swift! This book covers the fundamentals of Swift: Apple's modern programming language for iOS, iPadOS, macOS, watchOS and beyond")
+            HStack {
+                Text("📕")
+                Text("🖨️")
+                Text("💾")
+            }
+        }
+    }
+}
